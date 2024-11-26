5535aee9525d1e3208ee26abbe236d6f0644bf44
saj3sh <saj3sh@gmail.com>
2024-11-26 12:02:29 -0600

    chore: ui fixes

--- streamlit_components/page_styles.py
+++ streamlit_components/page_styles.py
@@ -7,14 +7,14 @@ def format_page_styles(st):
     header {
         display:none !important;
     }
-    .stChatMessage:nth-child(odd) {
-        flex-direction:row-reverse;
-        min-width:50%;
-        align-self:flex-end;
-    }
-    .stChatMessage:nth-child(odd) > div:first-child {
-    display:none !important;
-    }
+    # .stChatMessage:nth-child(odd) {
+    #     flex-direction:row-reverse;
+    #     min-width:50%;
+    #     align-self:flex-end;
+    # }
+    # .stChatMessage:nth-child(odd) > div:first-child {
+    # display:none !important;
+    # }
     </style>
     """
     st.markdown(page_style, unsafe_allow_html=True)
